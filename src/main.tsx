c2bf8ee8d0860231112cdc54dc2d665e60985bd7
emmanuel <nerdyemmanuel@gmail.com>
2024-06-22 19:21:07 +0100

    jhgf

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+const manifestUrl = 'https://emmanuelnwogbo.github.io/my-twa/tonconnect-manifest.json';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <App />
+  </TonConnectUIProvider>,
+)
